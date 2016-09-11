f2d5f35cf25729c7a5197db6c2a2c87f2177b4ad
lwiosbystep <1286594907@qq.com>
2016-09-11 11:41:01 +0800

    刷新时视图
    呃呃

---
+++ bossreferesh/RefreshAnimationView.swift
@@ -0,0 +1,58 @@
+//
+//  RefreshAnimationView.swift
+//  bossreferesh
+//
+//  Created by lotawei on 16/9/11.
+//  Copyright © 2016年 lotawei. All rights reserved.
+//
+
+import UIKit
+
+class RefreshAnimationView: UIView {
+    
+    var    currentvalue:CGFloat = 0  {
+        didSet{
+            
+           self.setNeedsDisplay()
+            print(currentvalue)
+        }
+    }
+    var   ori:CGPoint!
+    var  benginangle:CGFloat = CGFloat.init(-M_PI)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor =  UIColor.whiteColor()
+        let     w = frame.size.height > frame.size.width  ? frame.size.height:frame.size.width
+        self.frame.size  = CGSizeMake(w, w)
+        ori  = frame.origin
+    }
+    
+    
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        let   ref = UIGraphicsGetCurrentContext()
+        
+        
+        drawmorearc(ref!,currentv: currentvalue,begin:CGFloat(-M_PI))
+        
+        
+        
+    }
+   
+    func drawmorearc(ref:CGContext,currentv:CGFloat,begin:CGFloat)  {
+        CGContextSetRGBStrokeColor(ref, 0, 0, 1, 1)
+        CGContextSetLineWidth(ref, 5)
+        benginangle = CGFloat.init(begin+currentv)
+        CGContextAddArc(ref, frame.size.width/2, frame.size.height/2, frame.size.height/2-5, begin, CGFloat.init(begin+currentv), 0)
+        
+        CGContextStrokePath(ref)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
--- bossreferesh/ViewController.swift
+++ bossreferesh/ViewController.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+    
+    var   tableview:UITableView!
+    var   re:RefreshAnimationView!
+    var   lastoffsety:CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        
+       re = RefreshAnimationView(frame:CGRectMake(100,200,50,80))
+        re.center = self.view.center
+        view.addSubview(re)
+        
+        //演示简单加个tableview
+        tableview = UITableView.init(frame: CGRectMake(0, 0, 200, 150), style: .Plain)
+        tableview.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableview.delegate = self
+        tableview.dataSource = self
+        view.addSubview(tableview)
+        tableview.backgroundColor = UIColor.yellowColor()
+        
+        tableview.center = re.center
+        tableview.frame.origin.y = re.center.y +  90
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return  1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return   6
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var   cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        if  cell == nil
+        {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = "数据"
+        return  cell!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        
+        
+        if scrollView.contentOffset.y < 0  {
+            let ar = -scrollView.contentOffset.y
+            re.currentvalue = ar/5
+           
+           
+            if lastoffsety > -scrollView.contentOffset.y {
+                re.center.y -= ar/50
+            }
+            else
+            {
+                re.center.y += ar/50
+            }
+             lastoffsety = -scrollView.contentOffset.y
+        }
+        else  if  scrollView.contentOffset.y == 0
+        {
+             re.currentvalue = 0
+            re.center  = self.view.center
+        }
+        
+    }
 
 }
 
